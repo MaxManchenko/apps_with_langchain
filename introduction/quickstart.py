6bef597aff8e43678c832ee0c69585e4db6803cc
MaxManchenko <manchenko.se@gmail.com>
2023-08-18 19:15:04 +0300

    Commit before creating a new "quikcstart" branch

--- introduction/quickstart.py
+++ introduction/quickstart.py
@@ -8,21 +8,25 @@ load_dotenv(find_dotenv())
 # --------------------------------------------------------------
 # LLMs: Get predictions from a language model
 # --------------------------------------------------------------
-question = "Who won the FIFA World Cup in the year 1994? "
+# question = "Who won the FIFA World Cup in the year 1994? "
 
-template = """Question: {question}
+# template = """Question: {question}
 
-Answer: Let's think step by step."""
+# Answer: Let's think step by step."""
 
-prompt = PromptTemplate(template=template, input_variables=["question"])
+product = "Smart Apps using Large Language Models (LLMS)"
+
+template = "What is a good name for a company that makes {product}"
+
+prompt = PromptTemplate(template=template, input_variables=["product"])
 
 # --------------------------------------------------------------
 # LLMs: Get predictions from a language model
 # --------------------------------------------------------------
 repo_id = "google/flan-t5-xxl"
 
-llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0.1})
+llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0.5})
 
 llm_chain = LLMChain(prompt=prompt, llm=llm)
 
-print(llm_chain.run(question))
+print(llm_chain.run(product))
