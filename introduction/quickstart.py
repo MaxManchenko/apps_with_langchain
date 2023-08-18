7a7352ce7e4f0a5d37653f33095447311dcbf857
MaxManchenko <manchenko.se@gmail.com>
2023-08-18 21:58:13 +0300

    On the 'Agent step'

--- introduction/quickstart.py
+++ introduction/quickstart.py
@@ -1,32 +1,71 @@
 from dotenv import find_dotenv, load_dotenv
-from langchain import HuggingFaceHub
-from langchain import PromptTemplate, LLMChain
 
+from langchain import HuggingFaceHub
+from langchain import PromptTemplate, LLMChain, ConversationChain
+from langchain.chains import LLMChain
+from langchain.agents import load_tools, initialize_agent, AgentType
+from langchain.agents.load_tools import get_all_tool_names
+
+# Load environment variables
 load_dotenv(find_dotenv())
 
 
 # --------------------------------------------------------------
 # LLMs: Get predictions from a language model
 # --------------------------------------------------------------
-# question = "Who won the FIFA World Cup in the year 1994? "
-
-# template = """Question: {question}
-
-# Answer: Let's think step by step."""
-
-product = "Smart Apps using Large Language Models (LLMS)"
-
+# Prompt templates
+product = "Smart Apps using Large Language Models (LLMs)"
 template = "What is a good name for a company that makes {product}"
-
 prompt = PromptTemplate(template=template, input_variables=["product"])
 
-# --------------------------------------------------------------
-# LLMs: Get predictions from a language model
-# --------------------------------------------------------------
-repo_id = "google/flan-t5-xxl"
-
-llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0.5})
-
+# Initialise and run the LLM
+repo_id = "google/flan-t5-xxl"  # https://huggingface.co/google/flan-t5-xxl
+llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0.6})
 llm_chain = LLMChain(prompt=prompt, llm=llm)
-
 print(llm_chain.run(product))
+
+# --------------------------------------------------------------
+# Memory: Add State to Chains and Agents
+# --------------------------------------------------------------
+repo_id = "google/flan-t5-xxl"  # https://huggingface.co/google/flan-t5-xxl
+llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0.6})
+conversation = ConversationChain(llm=llm, verbose=True)
+
+output = conversation.predict(input="Hi there!")
+print(output)
+
+output = conversation.predict(input="Nice to meet you! What's your name?")
+print(output)
+
+# --------------------------------------------------------------
+# Chains: Combine LLMs and prompts in multy-step workflow
+# --------------------------------------------------------------
+# Prompt templates
+product = "AI chatbots for dental offices"
+template = "What is a good name for a company that makes {product}?"
+prompt = PromptTemplate(template=template, input_variables=["product"])
+
+# Initialise and run the LLM
+repo_id = "google/flan-t5-xxl"  # https://huggingface.co/google/flan-t5-xxl
+llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0.6})
+
+chain = LLMChain(llm=llm, prompt=prompt)
+print(chain.run(product))
+
+# --------------------------------------------------------------
+# Agents: Dynamically call chains based on user input
+# --------------------------------------------------------------
+# Prompt
+prompt = "In what year was Python released and who is original creator?"
+
+# Initialize and run the LLM
+repo_id = "google/flan-t5-xxl"  # https://huggingface.co/google/flan-t5-xxl
+llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0.6})
+
+get_all_tool_names()
+tools = load_tools(["wikipedia", "llm-math"], llm=llm)
+
+# Initialize an agent with the tools, the LLM, and the agent type
+agent = AgentType.ZERO_SHOT_REACT_DESCRIPTION
+agent_chain = initialize_agent(tools, llm=llm, agent=agent, verbose=True)
+print(agent_chain.run(prompt))
