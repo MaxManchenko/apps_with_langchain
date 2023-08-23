3509615e46d6b8476aa96ca246e57494c27eb5dd
MaxManchenko <manchenko.se@gmail.com>
2023-08-23 15:51:09 +0300

    The final version of the 'YouTubeAgent' is done

--- src/models/youtube_agent.py
+++ src/models/youtube_agent.py
@@ -1,26 +1,215 @@
 import json
 import logging
-import time
+import textwrap
 from dotenv import find_dotenv, load_dotenv
 
-from langchain import HuggingFaceHub, PromptTemplate
+from langchain import HuggingFaceHub
+from langchain import PromptTemplate, LLMChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import YoutubeLoader
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
+from langchain.prompts.prompt import PromptTemplate
 from langchain.memory import ConversationBufferMemory
 
-# Set the logging level
-logging.basicConfig(level=logging.INFO)
+from src.utils.query_runner import run_query
+
+
+# Load environment variables
+load_dotenv(find_dotenv())
+
+# Initialize embedings
+emb_model_name = "sentence-transformers/all-mpnet-base-v2"
+emb_model_kwargs = {"device": "cpu"}
+encode_kwargs = {"normalize_embeddings": False}
+embeddings = HuggingFaceEmbeddings(
+    model_name=emb_model_name,
+    model_kwargs=emb_model_kwargs,
+    encode_kwargs=encode_kwargs,
+)
+
+
+def create_db_from_youtube_video_url(video_url):
+    """The function embeds unstructured data and store the resulting embedding vectors
+    in a Vector Sotore databse for further similarity verctor search.
+
+    Args:
+        video_url (url): A link to the YouTube video
+
+    Returns:
+        VectorStore: Return vector store database initialized from documents and embeddings
+    """
+
+    # Load YouTube transcripts
+    loader = YoutubeLoader.from_youtube_url(video_url)
+    transcript = loader.load()
+
+    # Split the transcript into chunks, embed each chunk and load it into the vector store
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=100)
+    docs = text_splitter.split_documents(transcript)
+    vector_db = FAISS.from_documents(docs, embeddings)
+    return vector_db
+
+
+def get_response_from_query(vector_db, query, k=3):
+    """
+    LLMs can handle limited number of tokens (1024, 2048, 4096 and so on).
+    Setting the chunksize and k to 4 maximizes
+    the number of tokens to analyze.
+    """
+
+    docs = vector_db.max_marginal_relevance_search(query, k=k, fetch_k=5)
+    docs_page_content = " ".join([d.page_content for d in docs])
+
+    # Initialize the LLM model
+    repo_id = "google/flan-t5-xxl"  # https://huggingface.co/google/flan-t5-xxl
+    llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0.6})  # type: ignore
+
+    template = """
+        You are a helpful assistant that that can answer questions about youtube videos 
+        based on the video's transcript.
+        
+        Answer the following question: {question}
+        By searching for the following video transcript: {docs}
+        
+        Only use the factual information from the transcript to answer the question.
+        
+        If you feel like you don't have enough information to answer the question, say "I don't know".
+        
+        Your answers should be verbose and detailed.
+        """
+
+    prompt = PromptTemplate(input_variables=["question", "docs"], template=template)
+
+    chain = LLMChain(llm=llm, prompt=prompt)
+
+    response = chain.run(question=query, docs=docs_page_content)
+    response = response.replace("\n", "")
+    return response, docs, docs_page_content
+
+
+# Example usage:
+video_url = "https://www.youtube.com/watch?v=L_Guz73e6fw"
+vector_db = create_db_from_youtube_video_url(video_url)
+
+query = "What are they saying about Microsoft?"
+response, docs, docs_page_content = get_response_from_query(vector_db, query)
+print(textwrap.fill(response, width=85))
+print(textwrap.fill(docs_page_content, width=85))
+
+# ------------------------------------------------------
+# Use retriever for similarity rearch
+# ------------------------------------------------------
+
+
+def get_response_from_query_2(vector_db, query):
+    retriever = vector_db.as_retriever(
+        search_type="mmr", search_kwargs={"k": 3, "fetch_k": 5}
+    )
+
+    # Initialize the LLM model
+    repo_id = "google/flan-t5-xxl"  # https://huggingface.co/google/flan-t5-xxl
+    llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0.6})  # type: ignore
+
+    template = """
+        You are a helpful assistant that that can answer questions about youtube videos 
+        based on the video's transcript.
+        
+        Answer the following question: {question}
+        By searching for the following video transcript: {docs}
+        
+        Only use the factual information from the transcript to answer the question.
+        
+        If you feel like you don't have enough information to answer the question, say "I don't know".
+        
+        Your answers should be verbose and detailed.
+        """
+
+    prompt = PromptTemplate(input_variables=["question", "docs"], template=template)
+
+    chain = LLMChain(llm=llm, prompt=prompt)
+
+    response = chain.run(question=query, docs=retriever)
+    response = response.replace("\n", "")
+    return response, retriever
+
+
+# Example usage:
+query = "What are they saying about Microsoft?"
+response, retriever = get_response_from_query_2(vector_db, query)
+print(textwrap.fill(response, width=85))
+
+
+# ------------------------------------------------------
+# Using a custom prompt for condensing the question
+# ------------------------------------------------------
+video_url = "https://www.youtube.com/watch?v=L_Guz73e6fw"
+
+custom_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question. At the end of standalone question add this 'Answer the question in German language.' If you do not know the answer reply with 'I am sorry'."""
+CUSTOM_QUESTION_PROMPT = PromptTemplate.from_template(custom_template)
+
+
+# Initialize the LLM model
+repo_id = "google/flan-t5-xxl"  # https://huggingface.co/google/flan-t5-xxl
+llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0.0})  # type: ignore
+
+# Initialize embedings
+emb_model_name = "sentence-transformers/all-mpnet-base-v2"
+emb_model_kwargs = {"device": "cpu"}
+encode_kwargs = {"normalize_embeddings": False}
+embeddings = HuggingFaceEmbeddings(
+    model_name=emb_model_name,
+    model_kwargs=emb_model_kwargs,
+    encode_kwargs=encode_kwargs,
+)
+
+# Initialize loader
+loader = YoutubeLoader.from_youtube_url(video_url)
+transcript = loader.load()
+
+# Split the transcript into chunks, embed each chunk and load it into the vector store
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=100)
+docs = text_splitter.split_documents(transcript)
+vector_db = FAISS.from_documents(docs, embeddings)
+
+# Initialize memory
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+# Initialize the LLM model
+repo_id = "google/flan-t5-xxl"  # https://huggingface.co/google/flan-t5-xxl
+llm = HuggingFaceHub(repo_id=repo_id, model_kwargs={"temperature": 0.6})  # type: ignore
+
+# Initialize retriever
+retriever = vector_db.as_retriever(
+    search_type="mmr", search_kwargs={"k": 3, "fetch_k": 5}
+)
+
+qa = ConversationalRetrievalChain.from_llm(
+    llm=llm,
+    retriever=retriever,
+    condense_question_prompt=CUSTOM_QUESTION_PROMPT,
+    chain_type="stuff",
+    memory=memory,
+)
+
+# Example usage:
+query = "What are they saying about Microsoft?"
+result = qa({"question": query})
+
+
+# ------------------------------------------------------
+# YouTubeAgent class
+# ------------------------------------------------------
 
 
 class YouTubeAgent:
     """_summary_"""
 
     load_dotenv(find_dotenv())  # Load environment variables
+    logging.basicConfig(level=logging.INFO)
 
-    def __init__(self, video_url, query, config_path, *, k=3, fetch_k=5):
+    def __init__(self, video_url, config_path, *, k=3, fetch_k=5):
         """_summary_
 
         Args:
@@ -40,7 +229,6 @@ class YouTubeAgent:
                 Defaults to 5.
         """
         self.url = video_url
-        self.query = query
         self.k = k
         self.fetch_k = fetch_k
         self.config_path = config_path
@@ -53,8 +241,9 @@ class YouTubeAgent:
             self.llmodel = self.config.get("llm_hf")
             self.embedding = self.config.get("embeddings_hf")
             self.db = self.config.get("vector_db")
+            logging.info("The instance is built")
 
-    def run(self, max_retries=3):
+    def build_agent(self):
         logging.info("Starting the YouTubeAgent...")
         self.initialize_embeddings()
         self.initialize_llm()
@@ -64,23 +253,7 @@ class YouTubeAgent:
         self.get_youtube_transcripts()
         self.create_vector_db()
         self.initialize_retriever()
-        self.initialize_qa_chain()
-
-        # Retry loop
-        for retry in range(max_retries):
-            try:
-                result = self.run_query()
-                logging.info("YouTubeAgent run completed.")
-                return result  # Exit loop if successful
-
-            except ConnectionError as e:
-                logging.warning(f"ConnectionError occurred: {e}")
-                if retry < max_retries - 1:
-                    logging.info("Retrying the run_query method after a short delay...")
-                    time.sleep(3)  # Add a delay before retrying
-
-        logging.error(f"YouTubeAgent run failed after {max_retries} retries.")
-        return None
+        return self.initialize_qa_chain()
 
     def initialize_embeddings(self):
         """Initialize embeddings to convert data to vectors"""
@@ -151,29 +324,41 @@ class YouTubeAgent:
 
     def initialize_qa_chain(self):
         """Initialize a ConversationalRetrievalQA chain with a chat history component."""
-        self.qa = ConversationalRetrievalChain.from_llm(
+        qa = ConversationalRetrievalChain.from_llm(
             llm=self.llm,
             retriever=self.retriever,
             condense_question_prompt=self.CUSTOM_QUESTION_PROMPT,
             chain_type="stuff",
             memory=self.memory,
         )
-
-    def run_query(self):
-        """Get an answer to a question.
-
-        Returns:
-            dict: A dictionary with the question, chat_history and answer.
-        """
-        result = self.qa({"question": self.query})
-        return result
+        logging.info("The YouTubeAgent is redy to go.")
+        return qa
 
 
 # Example usage:
-config_path = "../../configs/youtube.json"
+config_path = "../configs/youtube.json"
 video_url = "https://www.youtube.com/watch?v=L_Guz73e6fw"
-query = "What are they saying about Microsoft?"
-yta = YouTubeAgent(video_url=video_url, query=query, config_path=config_path)
-result = yta.run()
-type(result)
-print(result)
+query1 = "What are they saying about Microsoft?"
+query2 = "What are they saying about OpenAI?"
+
+yta = YouTubeAgent(video_url=video_url, config_path=config_path)
+agent = yta.build_agent()
+result1 = run_query(agent, query1)
+result2 = run_query(agent, query2)
+
+
+# ------------------------------------------------------
+# Final test of the YouTubeAgent class
+# ------------------------------------------------------
+from models.YouTubeAgent import YouTubeAgent
+from src.utils.query_runner import run_query
+
+config_path = "configs/youtube.json"
+video_url = "https://www.youtube.com/watch?v=L_Guz73e6fw"
+query1 = "What are they saying about Microsoft?"
+query2 = "What are they saying about OpenAI?"
+
+yta = YouTubeAgent(video_url=video_url, config_path=config_path)
+agent = yta.build_agent()
+result1 = run_query(agent, query1)
+result2 = run_query(agent, query2)
--- src/utils/query_runner.py
+++ src/utils/query_runner.py
@@ -3,4 +3,17 @@ from tenacity import retry, stop_after_attempt, wait_fixed
 
 @retry(stop=stop_after_attempt(3), wait=wait_fixed(3))
 def run_query(qa_instance, query):
+    """Wrapper for additional 'retry' functionality:
+    make it possibe to call an Langchain instance several times
+    without waiting when an exception is raised.
+
+    Args:
+        qa_instance (int): A LangChain instance
+        query (_type_): _description_
+
+    Returns:
+        dict: {"question" : "your question",
+            "chat_hystory" : "list of chat history",
+            "answer" : "An LLM answer to your question"}.
+    """
     return qa_instance({"question": query})
